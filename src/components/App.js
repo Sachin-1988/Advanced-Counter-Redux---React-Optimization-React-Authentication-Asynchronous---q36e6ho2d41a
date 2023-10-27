90f8fd116820e3ea49b8970b1e3818fe88c84b6a
Vishal Sharma <785vishal443@gmail.com>
2023-10-27 23:04:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Counter from './Counter'
+import Login from './Login'
+import {useSelector} from 'react-redux'
+ 
 
 function App() {
-
+  const islogged = useSelector(state => state.islogged);
   return (
-    <div id='main'>
-      
+    <div className="App">
+    <Login />
+    {islogged ? <Counter />: <h3>Please Sign In</h3>}
     </div>
   );
 }
